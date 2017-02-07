18d256ca8209dc0548869ee28e7fc675e56ea3e4
Gerardo <gperegrina94@gmail.com>
2017-02-06 19:49:38 -0800

    Testing homepage

---
+++ index.php
@@ -0,0 +1,32 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="description" content="">
+		<meta name="author" content="">
+		<title>Fishing Supply</title>
+		<!-- Bootstrap Core CSS -->
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+		<!-- Custom CSS -->
+		<link href="css/shop-homepage.css" rel="stylesheet">
+		<!-- Text -->
+		<link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
+	</head>
+
+	<body>
+
+		<?php include_once('nav.php'); ?>
+
+		<?php include_once('title.php'); ?>
+
+		<?php include_once('center.php'); ?>
+		
+		<?php include_once('foot.php'); ?>
+
+	</body>
+
+</html>
